32351d8d08239cf500efd08035dad6a4289ebf85
Farhan <farhanshaikh11092002@gmail.com>
2025-12-15 17:20:56 +0530

    updated again

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Kidney-Disease-Classification-MLflow-DVC"
+REPO_NAME = "Kidney-Disease-Classification-DL"
 AUTHOR_USER_NAME = "DracyanF"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "farhanshaikh11092002@gmail.com"
